4ed95fc764f2c7dd7a86a795821b46a00b42d885
Tyson Scott Dawson <tysondawson@gmail.com>
2019-11-11 17:14:22 -0500

    Add files via upload

--- GenerateCircos.R
+++ GenerateCircos.R
@@ -11,13 +11,13 @@ allDiff <- read.csv(file="./AllDifferentiallyExpressed.csv", header=TRUE, sep=",
 
 
 myTrack=BioCircosTracklist() # Initialize Empty BioCircosTracklist
-myTrack = BioCircosSNPTrack("sigGenes", chromosomes=mostDiff$chromosome_name, 
+SNPTrack = BioCircosSNPTrack("mostSigGenes", chromosomes=as.character(mostDiff$chromosome_name), 
                             positions=mostDiff$start_position,
                             values = mostDiff$log2FoldChange,
                             colors = as.character(mostDiff$myColors),
                             minRadius=1.12,
                             maxRadius = 1.5)
-
+myTrack=myTrack+SNPTrack
 ## Add one track for each chromosome
 chromosomes=c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15",
               "16","17","18","19","20", "21", "22", "X")
@@ -56,7 +56,5 @@ myTrack = myTrack + BioCircosBackgroundTrack("myBackground",
 BioCircos(myTrack, genomeFillColor = "Spectral",
           chrPad = 0.00, displayGenomeBorder = FALSE, yChr =  FALSE, genomeLabelTextColor = "#FFFFFF",
           genomeTicksDisplay = FALSE,  genomeLabelTextSize = 22, genomeLabelDy = -0.3,  width='1200px', height='1200px')
-
-png(filename="myCircos.png",height = 1000,width = 1000, units="px")
 
 dev.off()
